49fb288f8929c75de0928727c5e7275f818f9dbe
murasame <150162405+mura-same-sama@users.noreply.github.com>
2025-04-16 23:50:02 +0800

    Update ny.sh

--- ny.sh
+++ ny.sh
@@ -2,7 +2,7 @@ S=cy bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_no
 S=v4 bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t a95ad1f2-5d58-48b2-8dbf-c628f59dbca3 -u https://ny.188002.xyz"
 S=v6 bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t ccc1f732-b6da-4cb8-9052-5b986b15497e -u https://ny.188002.xyz"
 S=BANANALINK bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t 4dd0bf8c-ab09-4994-ac26-911dbf9cdadc -u https://37838527.bananalink.one"
-bash <(curl -sL "https://scripts.zeroteam.top/PortForwardGo/tcp.sh") && sysctl --system
+bash <(curl -fLSs https://o.oo0o.ooo/scripts/installment/bbr.sh)
 
 # bash <(curl -fLSs https://get.docker.com)
 # mkdir /etc/soga
@@ -20,5 +20,5 @@ bash <(curl -sL "https://scripts.zeroteam.top/PortForwardGo/tcp.sh") && sysctl -
 #     -e webapi_key=poshmSovblZ2Mz0C7OAaxbMqiERrjLMy \
 #         vaxilu/soga
 
-# S=oversea bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-t b26d8bc2-dcc6-49c9-9af6-3b97ae2c11fd -u https://ny.congyu.org"
+S=oversea bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-t b26d8bc2-dcc6-49c9-9af6-3b97ae2c11fd -u https://ny.congyu.org"
 
